db1c88059d47a010fc64d154775605f0bd376bf9
VanJay <wangwanjie1993@gmail.com>
2020-03-29 01:20:03 +0800

    Version 1.8.5 release

--- ImageCompress/AlertHelper.swift
+++ ImageCompress/AlertHelper.swift
@@ -10,7 +10,7 @@ import Foundation
 
 func showAlert(style _: NSAlert.Style, message: String, informativeText: String) {
     let alert = HDAlert(title: message, message: informativeText, style: .warning)
-    alert?.addCommonButton(withTitle: "确定", handler: { _ in
+    alert?.addCommonButton(withTitle: localizedButton(key: "confirm"), handler: { _ in
 
     })
     alert?.show(NSApplication.shared.keyWindow)
